94a19fa89979ab8789dc14856b24116eb1e564f3
Bartosz Cwiertka <bartek.cwiertka@gmail.com>
2022-04-19 20:17:19 +0200

    akutalizacja main.py o sobczaku

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello World!")
+
+print("rzygansko sransko jebansko")
+x = input("czy sobczak kurwom jes?: ")
+if x == "jes to prawdom":
+    print("zgadza sie zgadza")
+elif x == "niet":
+    print("pierdolisz huju farmazony")
+else:
+    print("co ty pierdolisz")
