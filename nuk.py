9f4338c1c12db190d50ab5030d07c72fd8567ac4
Bartosz Cwiertka <bartek.cwiertka@gmail.com>
2022-04-23 16:59:26 +0200

    zegar

--- nuk.py
+++ nuk.py
@@ -1,20 +1,26 @@
 import time 
 from tkinter import *
 
-
 root = Tk()
+root.title("Zegar")
 root.geometry("600x400")
+root.configure(background="black")
 
-def update():
+def timer():
     hour = time.strftime("%H")
     minute = time.strftime("%M")
     second = time.strftime("%S")
-    timer = f"{hour} {minute} {second}"
-    print(timer)
-    label.config(text=timer)
-    
-label = Label(root, text="", padx=20, pady=20)
+    day = time.strftime("%d")
+    month = time.strftime("%B")
+    label.config(text=hour + ":" + minute + ":" + second)
+    label2.config(text=f"It's {day}th day of {month}")
+    label.after(1000, timer)
+
+label = Label(root, text="", padx=20, pady=20, font=("Times", "40", "bold italic"), fg="white", background="black")
 label.pack()
-label.after(1000, update)
+label2 = Label(root, text="", font=("Times", "20", "bold italic"), fg="white", background="black")
+label2.pack()
+timer()
 
 root.mainloop()
+
